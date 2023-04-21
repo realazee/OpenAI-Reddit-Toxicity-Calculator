3e47022439e670d858b319b4f270e3e28c26ab5c
realazee <aaronbrian328@gmail.com>
2023-04-21 13:23:09 -0400

    working functionality

--- app.py
+++ app.py
@@ -4,25 +4,31 @@ import os
 import json
 import openai
 from flask import Flask, Response, request, render_template, redirect, url_for, Blueprint
+from flask_login import *
 import flask_login
 from flaskext.mysql import MySQL
-from oauth import getUserComments
+from oauth import *
+import sys
 
 mysql = MySQL()
 app = Flask(__name__, template_folder="templates", static_folder="static")
+app.secret_key = "super secret key"
 secretFile = open("clientsecret.json", "r")
 secrets = json.load(secretFile)
 openai.api_key = secrets['secretKey']
-print(openai.api_key)
 
 #authentication stuff
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_token):
+     user = User()
+     user.id = user_token
+     return user
+
 class User(flask_login.UserMixin):
 	pass
-
-
-
 
 @app.route("/", methods=["GET", "POST"])
 def redirToLogin():
@@ -45,16 +51,20 @@ def loginHelper():
 def redditHelper():
     username = request.form.get('username')
     password = request.form.get('password')
-    commentList = getUserComments(username, password, '')
-    print(commentList)
+
+    token = reddit_login(username, password)
+    if token == 'error':
+         return render_template("login.html", error="didn't work")
+
+    user = User()
+    user.id = token
+    login_user(user)
     return render_template("homepage.html")
 
 @app.route("/oauth", methods=["POST"])
 def oauthHelper():
     nameToCheck = request.form.get('nameToCheck')
-    redditUsername = secrets['redditUsername']
-    redditPassword = secrets['redditPassword']
-    commentList = getUserComments(redditUsername, redditPassword, nameToCheck)
+    commentList = getUserComments(nameToCheck, current_user.id)
 
     response = openai.Moderation.create(input=str(commentList))
     output = response["results"][0]
@@ -65,14 +75,13 @@ def oauthHelper():
     sexual_minors = str(output["category_scores"]["sexual/minors"])
     violence = str(output["category_scores"]["violence"])
     violence_graphic = str(output["category_scores"]["violence/graphic"])
-    print(output)
     return render_template("openAI.html", hate=hate, hate_threatening=hate_threatening, self_harm=self_harm, sexual=sexual, sexual_minors=sexual_minors, violence=violence, violence_graphic=violence_graphic)
     
 
 
 if __name__ == "__main__":    
     app.debug = True
-    app.run(port=5002)
-    webbrowser.open_new("127.0.0.1:5002")
+    app.run(port=5003)
+    webbrowser.open_new("127.0.0.1:5003")
 
     
--- oauth.py
+++ oauth.py
@@ -10,45 +10,25 @@ redditSecret = secrets['redditSecret']
 #given redditUsername of the user who is logged in, redditPassword of the user who is logged in, 
 #and searchedUser of the user whose comments we want to fetch, this function authenticates redditUsername 
 #and returns an array of comments left by searchedUser.
-def getUserComments(redditUsername, redditPassword, searchedUser):
-
-    if searchedUser == '':
-        searchedUser = redditUsername
+def getUserComments(searchedUser, token):
 
     client_auth = requests.auth.HTTPBasicAuth(redditId, redditSecret)
 
-    data = {
-        'grant_type': 'password',
-        'username': redditUsername,
-        'password': redditPassword,
-    }
-
     headers = {'User-Agent': 'cs411 reddit script'}
-
-    
-    res = requests.post('https://www.reddit.com/api/v1/access_token', auth=client_auth, data=data, headers=headers)
-
-    js = res.json()
-
-    token = js['access_token']
-
 
     headers['Authorization'] = 'bearer {}'.format(token)
     me = requests.get('https://oauth.reddit.com/api/v1/me', headers=headers)
 
-    print(me.json())
-
     #this fetches the reddit comments left by searchedUser, filters it to 10 comments and stores it into userComments as a csv.
     userComments = requests.get('https://oauth.reddit.com/u/'+searchedUser+'/comments', headers=headers, params={'limit': '`10`'})
-    commentResponse = userComments.json()
+    commentResponse = userComments
     #commentlist is an array of comments left by searchedUser, each element is one comment. This is ultimately what gets passed into openAI.
     #front end for this still needs to be done.
-    commentList = [post['data']['body'] for post in commentResponse['data']['children']]
+    commentList = [post['data']['body'] for post in commentResponse.json()['data']['children']]
 
     return commentList
-def accessToken(redditUsername, redditPassword):
-    if searchedUser == '':
-        searchedUser = redditUsername
+
+def reddit_login(redditUsername, redditPassword):
 
     client_auth = requests.auth.HTTPBasicAuth(redditId, redditSecret)
 
@@ -59,11 +39,12 @@ def accessToken(redditUsername, redditPassword):
     }
 
     headers = {'User-Agent': 'cs411 reddit script'}
-
     
     res = requests.post('https://www.reddit.com/api/v1/access_token', auth=client_auth, data=data, headers=headers)
 
     js = res.json()
 
-    token = js['access_token']
-    return token
+    if 'access_token' in js:
+        return js['access_token']
+    else:
+        return 'error'
