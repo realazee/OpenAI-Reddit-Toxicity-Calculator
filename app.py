6e0b29f535747a72e66f3467cef5c652d0bc6a6b
realazee <aaronbrian328@gmail.com>
2023-04-21 11:03:16 -0400

    partial login function

--- app.py
+++ app.py
@@ -3,28 +3,42 @@ import requests
 import os
 import json
 import openai
-from flask import Flask, Response, request, render_template, redirect, url_for
+from flask import Flask, Response, request, render_template, redirect, url_for, Blueprint
+import flask_login
+from flaskext.mysql import MySQL
 from oauth import getUserComments
 
+mysql = MySQL()
 app = Flask(__name__, template_folder="templates", static_folder="static")
 secretFile = open("clientsecret.json", "r")
 secrets = json.load(secretFile)
 openai.api_key = secrets['secretKey']
 print(openai.api_key)
 
+#authentication stuff
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+class User(flask_login.UserMixin):
+	pass
+
+
+
 
 @app.route("/", methods=["GET", "POST"])
 def redirToLogin():
     return redirect(url_for('loginHelper'))
 
+@flask_login.login_required
 @app.route("/home", methods=["GET", "POST"])
 def homeHelper():
     return render_template("homepage.html")
 
-
 #page doesnt exist yet. will be created later
 @app.route("/login", methods=["GET", "POST"])
 def loginHelper():
+    #if login successful, run flask_login.login_user(user) and login the user
+    #if login unsuccessful, redirect to login page
+    
     return render_template("login.html")
 
 @app.route("/redditAuth", methods=["GET", "POST"])
