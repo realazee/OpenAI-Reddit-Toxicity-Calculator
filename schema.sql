2155b8324b0b0e669f652558f608f029df792a4e
rithvik <rithvik@rntech.org>
2023-04-26 10:12:47 -0400

    Made some changes to the oauthhelper function to format some of the results and create gauges to display the data.

--- schema.sql
+++ schema.sql
@@ -4,3 +4,7 @@ USE toxicitycalc;
 DROP TABLE IF EXISTS Users CASCADE;
 DROP TABLE if EXISTS ScoreHistory CASCADE
 
+CREATE TABLE users (
+    username VARCHAR(50) UNIQUE NOT NULL,
+    results VARCHAR(200) NOT NULL
+);
