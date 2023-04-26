4937a45885b0815c002725ca66700b260ad0e9b1
realazee <aaronbrian328@gmail.com>
2023-04-26 11:55:01 -0400

    implemented database for search history and history.html

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 CREATE DATABASE IF NOT EXISTS toxicitycalc;
 USE toxicitycalc;
 
-DROP TABLE IF EXISTS Users CASCADE;
-DROP TABLE if EXISTS ScoreHistory CASCADE
+DROP TABLE IF EXISTS searchedUsers CASCADE;
 
-CREATE TABLE users (
+CREATE TABLE searchedUsers (
+    user_id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(50) UNIQUE NOT NULL,
-    results VARCHAR(200) NOT NULL
+    results VARCHAR(200) NOT NULL DEFAULT 0,
+    PRIMARY KEY (user_id)
 );
